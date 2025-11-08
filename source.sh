2a89ee83efe9a7ac9c5b5032dc79a738863f1c38
NRV2ouf <teorock76@yahoo.fr>
2025-11-09 00:10:38 +0100

    style: Added source.sh

---
+++ source.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source ./src/download.sh
+source ./src/folder.sh
+source ./src/mp3-metadata.sh
+source ./src/name-duplicate.sh
+source ./src/pair-remover.sh
+source ./src/space-trimmer.sh
+source ./src/utils.sh
